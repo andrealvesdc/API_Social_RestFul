bf5c9c2ca453c82da9a3f006ce8f84bd5333c8c9
Andre Alves <andrealvesdc@gmail.com>
2020-03-08 16:55:33 -0300

    Evitando retorno de informações nulas

--- socialbooksapi/src/main/java/com/andrealves/socialbooks/domain/Livro.java
+++ socialbooksapi/src/main/java/com/andrealves/socialbooks/domain/Livro.java
@@ -3,14 +3,30 @@ package com.andrealves.socialbooks.domain;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 public class Livro {
 	
+	@JsonInclude(Include.NON_NULL)
 	private Long id;
+	
+	@JsonInclude(Include.NON_NULL)
 	private String nome;
+	
+	@JsonInclude(Include.NON_NULL)
 	private String editora;
+	
+	@JsonInclude(Include.NON_NULL)
 	private String resumo;
-	private String autor;	
+	
+	@JsonInclude(Include.NON_NULL)
+	private String autor;
+	
+	@JsonInclude(Include.NON_NULL)
 	private Date publicacao;
+	
+	@JsonInclude(Include.NON_NULL)
 	private List<Comentarios> comentarios;
 	
 	public Livro() {
